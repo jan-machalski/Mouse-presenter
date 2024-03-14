001bc2a73e1f9a26a28b27ac96134011666c0bbb
jan-machalski <73649497+jan-machalski@users.noreply.github.com>
2024-03-15 00:35:01 +0100

    Delete lab2_3 directory

--- lab2_3/main.cpp
+++
@@ -1,9 +0,0 @@
-#define NOMINMAX
-#include <windows.h>
-#include "app.h"
-int WINAPI wWinMain(HINSTANCE instance,
-	HINSTANCE /*prevInstance*/,
-	LPWSTR /*command_line*/,
-	int show_command)
-{	app ap{ instance };
-	return ap.run(show_command);}
